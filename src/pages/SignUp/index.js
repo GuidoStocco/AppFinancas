8b41fb4574c913b5893e1a8945b9b0701ecdc1c3
Guido Stocco <guidostoccodev@gmail.com>
2025-05-12 21:27:35 -0300

    Wk:Pagina de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Platform} from 'react-native';
+import { Background } from './styles';
 
 
 export default function SignUp(){
     return(
-        <View>
+        <Background>
             <Text>Tela Cadastro</Text>
-        </View>
+        </Background>
     );
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -20,6 +20,9 @@ export default function AuthRoutes(){
             <AuthStack.Screen
             name='SignUp'
             component={SignUp}
+            options={{
+                headerShown: false
+            }}
             />
         </AuthStack.Navigator>
     );
