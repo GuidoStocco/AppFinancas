cc3dad2c2ff9335a292cd8c69509a410208f1460
Guido Stocco <guidostoccodev@gmail.com>
2025-05-22 20:11:25 -0300

    WK: Logando usuário

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 import { AuthContext } from '../contexts/auth';
